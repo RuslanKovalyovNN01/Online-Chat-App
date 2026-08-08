00f38d56689a7a49893cbfe4e8a6e75e660bb233
Kovalyov Ruslan <i-am-kovalyov-ruslan@yandex.ru>
2026-08-09 00:10:28 +0300

    add input

--- websocket_demo/src/main/java/com/example/websocket_demo/client/App.java
+++ websocket_demo/src/main/java/com/example/websocket_demo/client/App.java
@@ -13,9 +13,18 @@ public class App {
         SwingUtilities.invokeLater(new Runnable(){
             @Override
             public void run() {
+                String username = JOptionPane.showInputDialog(null, "Enter Username (Max: 16 Characters): ",
+                        "Chat Applitcation" , JOptionPane.QUESTION_MESSAGE);
+                if(username == null || username.isEmpty() || username.length() > 16){
+                    JOptionPane.showMessageDialog(null,
+                            "Invalid username",
+                            "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
                 ClientGUI clientGUI = null;
                 try {
-                    clientGUI = new ClientGUI("Ruslan");
+                    clientGUI = new ClientGUI(username);
                 } catch (ExecutionException e) {
                     throw new RuntimeException(e);
                 } catch (InterruptedException e) {
