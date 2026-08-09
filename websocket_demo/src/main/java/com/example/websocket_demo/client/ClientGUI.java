97330b679c99928fbaac3c61c5ba480326bb1e5c
Kovalyov Ruslan <i-am-kovalyov-ruslan@yandex.ru>
2026-08-09 17:21:35 +0300

    add scrolling panels

--- websocket_demo/src/main/java/com/example/websocket_demo/client/ClientGUI.java
+++ websocket_demo/src/main/java/com/example/websocket_demo/client/ClientGUI.java
@@ -7,10 +7,7 @@ import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
@@ -46,6 +43,12 @@ public class ClientGUI extends JFrame implements MessageListener{
                     }
                 }
 
+        });
+        addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                updateMessageSize();
+            }
         });
         getContentPane().setBackground(Utilities.PRIMARY_COLOR);
         addGUIComponents();
@@ -89,7 +92,7 @@ public class ClientGUI extends JFrame implements MessageListener{
 
         chatPanel.add(messagePanelScrollPane, BorderLayout.CENTER);
 
-        messagePanel.add(createChatMessageComponent(new Message("Ruslan", "Hello!")));
+        //messagePanel.add(createChatMessageComponent(new Message("Ruslan", "Hello!")));
 
 
         //JLabel message = new JLabel("Hi everyone");
@@ -138,7 +141,13 @@ public class ClientGUI extends JFrame implements MessageListener{
         usernameLabel.setForeground(Utilities.TEXT_COLOR);
         chatMessage.add(usernameLabel);
 
-        JLabel messageLabel = new JLabel(message.getMessage());
+        int width = (int) (getWidth() * 0.6);
+        JLabel messageLabel = new JLabel();
+        messageLabel.setText(
+                "<html><body style='width:" + width + "px'>" +
+                        message.getMessage() +
+                        "</body></html>"
+        );
         messageLabel.setFont(new Font("Inter", Font.PLAIN, 18));
         messageLabel.setForeground(Utilities.TEXT_COLOR);
         chatMessage.add(messageLabel);
@@ -174,5 +183,21 @@ public class ClientGUI extends JFrame implements MessageListener{
         connectedUsersPanel.add(usersListPanel);
         revalidate();
         repaint();
+    }
+    private void updateMessageSize() {
+        for(int i = 0; i < messagePanel.getComponents().length; i++){
+            Component component = messagePanel.getComponent(i);
+            if(component instanceof JPanel){
+                JPanel chatMessage = (JPanel) component;
+                if(chatMessage.getComponent(1) instanceof JLabel){
+                    JLabel messageLabel = (JLabel) chatMessage.getComponent(1);
+                    messageLabel.setText(
+                            "<html><body style='width:" + (0.60 * getWidth()) + "px'>" +
+                                    messageLabel.getText() +
+                                    "</body></html>"
+                    );
+                }
+            }
+        }
     }
 }
